c0916da2f2d456f47dfe677e3a8cd192b0483f56
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-11-15 13:12:31 -0500

    Added editor component

---
+++ src/finna/component/editor.cljs
@@ -0,0 +1,25 @@
+(ns finna.component.editor
+  (:require
+   [reagent.core :as r]
+   [re-frame.core :as re]))
+
+
+(defn c-editor []
+  (let []
+    (r/create-class
+     {:display-name "c-editor"
+      
+      :component-did-mount
+      (fn [this]
+        (let [el-node (r/dom-node this)]
+
+          ;; Instantiate Code Mirror Instance
+          (js/CodeMirror. 
+           el-node 
+           #js {:lineNumbers true
+                :scrollbarStyle "simple"
+                :mode "clojure"})))
+
+      :reagent-render
+      (fn []
+        [:div.codemirror.editor])})))
--- src/finna/page/main.cljs
+++ src/finna/page/main.cljs
@@ -4,12 +4,13 @@
    [re-frame.core :as re]
 
    ;; Components
-   [finna.component.main-header :refer [c-main-header]]))
+   [finna.component.main-header :refer [c-main-header]]
+   [finna.component.editor :refer [c-editor]]))
 
 (defn c-main []
   [:div.page.main {}
    [:div.panel.header [c-main-header]]
    [:div.panel.left "left"]
-   [:div.panel.content "content"]
+   [:div.panel.content [c-editor]]
    [:div.panel.right "right"]
    [:div.footer "footer"]])
