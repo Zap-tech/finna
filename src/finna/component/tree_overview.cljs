1addf27df9b7960d73a382dc71da787573975403
Benjamin Zaporzan <benzaporzan@gmail.com>
2020-01-10 01:40:01 -0500

    Started work on tree overview

---
+++ src/finna/component/tree_overview.cljs
@@ -0,0 +1,27 @@
+(ns finna.component.tree-overview
+  (:require
+   [reagent.core :as r]
+   [re-frame.core :as re]
+
+   [blueprint.core :refer [Icon Tree]]))
+
+
+(defn generate-tree-structure
+  []
+  (clj->js [{:id 0
+             :hasCaret true
+             :icon "folder-close"
+             :label ":stdlib.core"
+             :isExpanded true
+             :childNodes [{:id 1 :icon "function" :label "and"}
+                          {:id 1 :icon "function" :label "or"}]}]))
+
+
+(defn c-tree-overview
+  [{:keys [] :as opts}]
+  [:div.tree-overview
+   [:div.search-input
+    [:input {:type "text" :placeholder "Search"}]
+    [:> Icon {:icon "search"}]]
+   [:div.tree-container
+    [:> Tree {:contents (generate-tree-structure)}]]])
--- src/finna/page/main.cljs
+++ src/finna/page/main.cljs
@@ -5,13 +5,15 @@
 
    ;; Components
    [finna.component.main-header :refer [c-main-header]]
-   [finna.component.editor :refer [c-editor]]))
+   [finna.component.editor :refer [c-editor]]
+   [finna.component.tree-overview :refer [c-tree-overview]]))
 
 (defn c-main []
   [:div.page.main {}
    [:div.panel.header [c-main-header]]
-   [:div.panel.left "left"]
+   [:div.panel.left [c-tree-overview]]
    [:div.panel.content [c-editor]]
-   [:div.panel.right "right"]
+   [:div.panel.right "console"]
+   [:div.panel.bottom "output"]
    [:div.panel.footer
     "© 2019 Zaptech Enterprises"]])
